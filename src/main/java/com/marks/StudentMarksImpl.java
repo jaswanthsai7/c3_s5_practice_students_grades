156cde571045cf78015239bf1b1aa010a9d0e6e9
Jaswanth Sai <jasvanthsai7@gmail.com>
2022-08-30 13:11:42 +0530

    initialized the values by calling methods and print the values

--- src/main/java/com/marks/StudentMarksImpl.java
+++ src/main/java/com/marks/StudentMarksImpl.java
@@ -7,6 +7,14 @@ package com.marks;
 
 public class StudentMarksImpl {
     public static void main(String[] args) {
+        // declare and initialise an object
         StudentMarks studentMarks = new StudentMarks();
+        // call the methods and print the values
+        int businessPathWay = studentMarks.calculateMarks(10, 20, 30);
+        System.out.println("businessPathWay = " + businessPathWay);
+        int medicalPathWay = studentMarks.calculateMarks(20, 50, 70, 30);
+        System.out.println("medicalPathWay = " + medicalPathWay);
+        int nonMedicalPathWay = studentMarks.calculateMarks(70, 80, 90);
+        System.out.println("nonMedicalPathWay = " + nonMedicalPathWay);
     }
 }
